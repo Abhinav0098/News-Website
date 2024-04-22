2ee0ec3ac4206b7cfdf1ab5fb3a5987c48f1e29c
Abhinav0098 <167759872+Abhinav0098@users.noreply.github.com>
2024-04-22 19:58:17 +0530

    Update test.js

--- test.js
+++ test.js
@@ -37,8 +37,6 @@ let newsCategory = [
   "technology",
   "startup",
   "entertainment",
-  "miscellaneous",
-  "hatke",
   "science",
   "automobile",
 ];
@@ -54,7 +52,7 @@ getNews("all");
 function getNews(newsCategoryName) {
   xhr.open(
     "GET",
-    https://inshortsapi.vercel.app/news?category=${newsCategoryName},
+    `https://inshortsapi.vercel.app/news?category=${newsCategoryName}`,
     true
   );
 
